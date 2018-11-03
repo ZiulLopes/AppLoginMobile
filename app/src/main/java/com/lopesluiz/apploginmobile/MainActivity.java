826a960f02544522ffe55ca0fa11db190f824581
Ziullopes <lopesluiz_@hotmail.com>
2018-11-03 09:58:08 -0300

    First Files App

---
+++ app/src/main/java/com/lopesluiz/apploginmobile/MainActivity.java
@@ -0,0 +1,32 @@
+package com.lopesluiz.apploginmobile;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+
+public class MainActivity extends AppCompatActivity {
+
+    private EditText Loginname;
+    private EditText Password;
+    private Button Login;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        Loginname = (EditText)findViewById(R.id.edtLogin);
+        Password = (EditText)findViewById(R.id.edtPassord);
+        Login = (Button)findViewById(R.id.btnLogin);
+
+        Login.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                // Do something
+            }
+        });
+    }
+}
