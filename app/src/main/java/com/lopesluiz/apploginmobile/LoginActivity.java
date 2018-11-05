5e8970e35745db742c1364fa42eca7b8b7e66d33
ZiulLopes <=>
2018-11-05 19:08:33 -0200

    Edit layouts and refactor name java activity files

--- app/src/main/java/com/lopesluiz/apploginmobile/LoginActivity.java
+++ app/src/main/java/com/lopesluiz/apploginmobile/LoginActivity.java
@@ -2,12 +2,10 @@ package com.lopesluiz.apploginmobile;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.util.Log;
-import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 
-public class MainActivity extends AppCompatActivity {
+public class LoginActivity extends AppCompatActivity {
 
     private EditText Loginname;
     private EditText Password;
@@ -16,7 +14,7 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_login);
 
 
     }
